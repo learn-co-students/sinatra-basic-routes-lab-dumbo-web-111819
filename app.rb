400ed828cc48d7582245d6b51aacaaaffaf455a0
ajshemi <akpojotor@yahoo.com>
2019-12-09 13:42:07 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Shemi"
+    end
+
+    get '/hometown' do
+        "My hometown is Warri"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Under my umbrella"
+    end
 end
